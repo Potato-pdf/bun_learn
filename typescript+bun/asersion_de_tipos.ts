7e8722d4b655494f0097f53fe05af1b170271536
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-10-15 13:29:01 -0600

    feat: typeof

--- typescript+bun/asersion_de_tipos.ts
+++ typescript+bun/asersion_de_tipos.ts
@@ -1,5 +1,15 @@
 const canvas = document.getElementById("span") as HTMLCanvasElement;
-
+//instanceof es para clases
 if (canvas instanceof HTMLCanvasElement) {
     const ctx = canvas.getContext("2d");
 }
+
+// typeof es para tipos primitivos
+function printId(id: number | string) {
+    if (typeof id === "string") {
+        console.log(id.toUpperCase());
+    } else {
+        console.log(id);
+    }
+}
+
