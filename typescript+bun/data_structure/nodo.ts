2afbc0dfd9dd3d7c7f7d45c8a436ce4b2b3be3fd
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-10-17 20:50:21 -0600

    feat: set nodo

--- typescript+bun/data_structure/nodo.ts
+++ typescript+bun/data_structure/nodo.ts
@@ -3,10 +3,12 @@ import {nodo} from "./interfaces/nodo"
 class nodo_class<T> implements nodo<T> {
     data: T;
     next: nodo_class<T> | null;
-
-    constructor(data: T, next: nodo_class<T> | null = null, head:null, tail:null) {
+    constructor(data: T, next: nodo_class<T> | null) {
         this.data = data;
         this.next = next;
 }
-
+    set next_node(next_node : nodo_class<T>|null){
+        this.next=next_node;
+        
+    }
 }
