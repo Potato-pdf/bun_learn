2f9c2aaf3a40ce81e70c5cc43590b2a6d0c57a02
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-10-21 20:18:14 -0600

    feat: añadir nodos

--- typescript+bun/data_structure/nodo.ts
+++ typescript+bun/data_structure/nodo.ts
@@ -3,12 +3,18 @@ import {nodo} from "./interfaces/nodo"
 class nodo_class<T> implements nodo<T> {
     data: T;
     next: nodo_class<T> | null;
-    constructor(data: T, next: nodo_class<T> | null) {
+    constructor(data: T) {
         this.data = data;
-        this.next = next;
+        this.next = null;
 }
-    set next_node(next_node : nodo_class<T>|null){
-        this.next=next_node;
+    set_next_node(next_node : nodo_class<T>|null){
+        if (next_node instanceof nodo_class || next_node === null) 
+            this.next=next_node;
         
     }
 }
+
+const book1: nodo_class<string> = new nodo_class<string>('Nombre1');
+const book: nodo_class<string>= new nodo_class<string>("nombre2");
+
+book.set_next_node(book1)
