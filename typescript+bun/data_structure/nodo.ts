f9a4d2e5ce643c51f56c4ccf9688e6cb9b462dd7
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-10-24 19:05:17 -0600

    feat: nodo terminado

--- typescript+bun/data_structure/nodo.ts
+++ typescript+bun/data_structure/nodo.ts
@@ -12,9 +12,24 @@ class nodo_class<T> implements nodo<T> {
             this.next=next_node;
         
     }
+    get_next_nodo(){
+        return this.next
+    }
+    get_data(){
+        return this.data
+    }
 }
 
-const book1: nodo_class<string> = new nodo_class<string>('Nombre1');
+const book1: nodo_class<string> = new nodo_class<string>('nombre1');
 const book: nodo_class<string>= new nodo_class<string>("nombre2");
+const book2: nodo_class<string>= new nodo_class<string>("nombre3");
 
 book.set_next_node(book1)
+book1.set_next_node(book2)
+
+let current_node : nodo_class<string> | null = book
+
+while(current_node){
+    console.log(current_node.get_data())
+    current_node = current_node.get_next_nodo()
+}
