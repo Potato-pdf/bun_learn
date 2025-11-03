e31356bc61b7ad9473820f3c9d9f2f3a809a56de
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-03 11:18:48 -0600

    feat: healthcheck controller

--- Hono/hono-app/src/infrestructure/controllers/health.check.controller.ts
+++ Hono/hono-app/src/infrestructure/controllers/health.check.controller.ts
@@ -1,5 +1,9 @@
-import { controller } from "./controller";
-
+import { controller, response } from "./controller";
+import HttpStatus  from "http-status";
+import {Context, TypedResponse} from "hono";
+import { StatusCode } from "hono/utils/http-status";
 export default class health_check_controller implements controller{
-    
+    async run(c:Context): Promise<Response & TypedResponse<response,StatusCode, "json">>{
+        return c.json({ message: "ok" }, HttpStatus.OK)
+    }
 }
