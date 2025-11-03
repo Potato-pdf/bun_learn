12bfadc43d33f3cc2acfabb5d915f5e375ff2c18
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-03 12:33:39 -0600

    feat: decorator to intensify

--- Hono/hono-app/src/infrestructure/controllers/health.check.controller.ts
+++ Hono/hono-app/src/infrestructure/controllers/health.check.controller.ts
@@ -2,6 +2,9 @@ import { controller, response } from "./controller";
 import HttpStatus  from "http-status";
 import {Context, TypedResponse} from "hono";
 import { StatusCode } from "hono/utils/http-status";
+import { injectable } from "inversify";
+
+@injectable()
 export default class health_check_controller implements controller{
     async run(c:Context): Promise<Response & TypedResponse<response,StatusCode, "json">>{
         return c.json({ message: "ok" }, HttpStatus.OK)
