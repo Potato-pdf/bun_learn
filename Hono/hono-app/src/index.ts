b674a937f5208d17b652c16bb9e66b63eeaf1f08
Potato-pdf <175372743+Potato-pdf@users.noreply.github.com>
2025-11-03 12:33:27 -0600

    add rouytes to index

--- Hono/hono-app/src/index.ts
+++ Hono/hono-app/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono';
 import "reflect-metadata";
-const app = new Hono()
+import { register_routes } from './infrestructure/routes';
+import { logger } from 'hono/logger';
+const app = new Hono().basePath("saltillo")
 
-app.get('/', (c) => {
-  return c.text('Gente pendja')
-})
+app.use("*", logger())
+
+register_routes(app);
 
 export default app
